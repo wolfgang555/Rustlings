2a5a610e0b6a7bb973db1a0e7fd894b81fc765d4
wolfgang555 <15118507+wolfgang555@user.noreply.gitee.com>
2025-01-18 07:12:27 +0000

    solved

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(input: i32) -> i32 {
+    if input >= 41 {
+        return input;
+    }
+    input * 2
+}
 
 // Don't modify this function!
 #[test]
